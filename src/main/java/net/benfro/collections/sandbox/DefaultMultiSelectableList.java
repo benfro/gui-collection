b376b02d04418f5e1ea1bbd34ebe8892d4070363
Bengt-Erik Fröberg <>
2020-10-08 23:39:21 +0200

    Began DefaultMultiSelectableList implementation

--- src/main/java/net/benfro/collections/sandbox/DefaultMultiSelectableList.java
+++ src/main/java/net/benfro/collections/sandbox/DefaultMultiSelectableList.java
@@ -1,71 +1,84 @@
 package net.benfro.collections.sandbox;
 
-import ca.odell.glazedlists.BasicEventList;
 import ca.odell.glazedlists.EventList;
+import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import net.benfro.collections.DefaultObservableList;
+import net.benfro.collections.SelectableList;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.util.*;
+import java.util.stream.Collectors;
 
-public class DefaultMultiSelectableList<E> extends DefaultObservableList<DefaultMultiSelectableList.SelectableWrapper<E>> implements MultiSelectableList<E> {
+public class DefaultMultiSelectableList<E> extends DefaultObservableList<E> implements MultiSelectableList<E> {
 
-   static class SelectableWrapper<E> {
-      public final E wrapped;
-      public boolean selected;
+   private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+   private final Set<Integer> selected = Sets.newConcurrentHashSet();
 
-      public SelectableWrapper(E wrapped) {
-         this.wrapped = wrapped;
-      }
-
-      @Override
-      public boolean equals(Object o) {
-         if (this == o) return true;
-         if (o == null || getClass() != o.getClass()) return false;
-         SelectableWrapper<?> that = (SelectableWrapper<?>) o;
-         return Objects.equals(wrapped, that.wrapped);
-      }
-
-      @Override
-      public int hashCode() {
-         return Objects.hash(wrapped);
-      }
-   }
-
-   //public  static DefaultMultiSelectableList of() {
-   //   EventList<SelectableWrapper> eventList =  GlazedLists.even
-   //}
-
-   protected DefaultMultiSelectableList(EventList<SelectableWrapper<E>> backingList) {
+   protected DefaultMultiSelectableList(EventList<E> backingList) {
       super(backingList, false);
-      backingList = new BasicEventList<>(backingList.getPublisher(), backingList.getReadWriteLock());
    }
 
    @Override
    public void selectElements(E... elements) {
-      backingList.getReadWriteLock().writeLock().lock();
+      backingList.getReadWriteLock().readLock().lock();
       try {
          ArrayList<E> elementsToSelect = Lists.newArrayList(elements);
          if (backingList.containsAll(elementsToSelect)) {
-            //elementsToSelect.forEach(e -> selectedRegistry.add(backingList.indexOf(e)));
+            elementsToSelect.forEach(e -> {
+               int index = backingList.indexOf(e);
+               selected.add(index);
+               support.fireIndexedPropertyChange(SelectableList.SELECTED_ELEMENT_PROPERTY, index, null, e);
+            });
          } else {
             throw new IllegalArgumentException("One or more elements aren't contained in this list and thus not selectable");
          }
       } finally {
-         backingList.getReadWriteLock().writeLock().unlock();
+         backingList.getReadWriteLock().readLock().unlock();
       }
 
    }
 
    @Override
    public void deselectElements(E... elements) {
-
+      backingList.getReadWriteLock().readLock().lock();
+      try {
+         ArrayList<E> elementsToDeselect = Lists.newArrayList(elements);
+         elementsToDeselect.forEach(e -> {
+            int index = backingList.indexOf(e);
+            if (index >= 0) {
+               if (selected.remove(index)) {
+                  support.fireIndexedPropertyChange(SelectableList.SELECTED_ELEMENT_PROPERTY, index, e, null);
+               }
+               ;
+            }
+         });
+      } finally {
+         backingList.getReadWriteLock().readLock().unlock();
+      }
    }
 
    @Override
    public void selectIndices(int... indices) {
+      backingList.getReadWriteLock().readLock().lock();
+      Optional<Integer> max = Arrays.stream(indices).boxed().max(Integer::compare);
+      Optional<Integer> min = Arrays.stream(indices).boxed().min(Integer::compare);
+      max.ifPresent(maxVal -> Preconditions.checkArgument(maxVal < backingList.size()));
+      min.ifPresent(minVal -> Preconditions.checkArgument(minVal >= 0 && minVal < backingList.size()));
 
+      try {
+         Arrays.stream(indices).boxed().forEach(idx -> {
+            Optional.ofNullable(backingList.get(idx)).ifPresent(elem -> {
+               if (selected.add(idx)) {
+                  support.fireIndexedPropertyChange(SelectableList.SELECTED_ELEMENT_PROPERTY, idx, null, elem);
+               }
+            });
+         });
+      } finally {
+         backingList.getReadWriteLock().readLock().unlock();
+      }
    }
 
    @Override
@@ -85,11 +98,24 @@ public class DefaultMultiSelectableList<E> extends DefaultObservableList<Default
 
    @Override
    public List<E> getSelectedElements() {
-      return null;
+      return selected.stream().map(i -> backingList.get(i)).collect(Collectors.toList());
    }
 
    @Override
    public boolean isElementSelected(E element) {
-      return false;
+      backingList.getReadWriteLock().readLock().lock();
+      try {
+         return selected.contains(backingList.indexOf(element));
+      } finally {
+         backingList.getReadWriteLock().readLock().unlock();
+      }
+   }
+
+   public void addPropertyChangeListener(PropertyChangeListener listener) {
+      support.addPropertyChangeListener(listener);
+   }
+
+   public void removePropertyChangeListener(PropertyChangeListener listener) {
+      support.removePropertyChangeListener(listener);
    }
 }
--- src/test/java/net/benfro/collections/sandbox/DefaultMultiSelectableListTest.java
+++ src/test/java/net/benfro/collections/sandbox/DefaultMultiSelectableListTest.java
@@ -1,16 +1,95 @@
 package net.benfro.collections.sandbox;
 
+import ca.odell.glazedlists.GlazedLists;
+import com.google.common.collect.Lists;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @DisplayName("A DefaultMultiSelectableList")
 class DefaultMultiSelectableListTest {
+
+   @Captor
+   ArgumentCaptor<PropertyChangeEvent> eventCaptor;
+   @Mock
+   PropertyChangeListener mock;
+   DefaultMultiSelectableList<String> instance;
+
+   @BeforeEach
+   void setUp() {
+      MockitoAnnotations.openMocks(this);
+      instance = new DefaultMultiSelectableList<>(GlazedLists.eventListOf("A", "B", "C", "D", "E", "F"));
+      instance.addPropertyChangeListener(mock);
+   }
 
    @Test
    @DisplayName("should have a static factory method creating an empty instance")
    void testCreateEmpty() {
       assertTrue(DefaultMultiSelectableList.of().isEmpty());
+   }
+
+   @Test
+   void testState() {
+      assertEquals(6, instance.size());
+      assertFalse(instance.isEmpty());
+      assertTrue(instance.getSelectedElements().isEmpty());
+   }
+
+   @Test
+   void testSelectOneElement() {
+      instance.selectElements("E");
+      assertEquals(Collections.singletonList("E"), instance.getSelectedElements());
+
+      verify(mock, times(1)).propertyChange(eventCaptor.capture());
+   }
+
+   @Test
+   void testSelectOneIndex() {
+      instance.selectIndices(4);
+      assertEquals(Collections.singletonList("E"), instance.getSelectedElements());
+
+      verify(mock, times(1)).propertyChange(eventCaptor.capture());
+   }
+
+   @Test
+   void testSelectMoreElement() {
+      instance.selectElements("E", "B", "F");
+      assertEquals(Lists.newArrayList("B", "E", "F"), instance.getSelectedElements());
+
+      verify(mock, times(3)).propertyChange(eventCaptor.capture());
+   }
+
+   @Test
+   void testDeselectOneElement() {
+      instance.selectElements("E");
+      instance.deselectElements("E");
+      assertTrue(instance.getSelectedElements().isEmpty());
+      verify(mock, times(2)).propertyChange(eventCaptor.capture());
+   }
+
+   @Test
+   void testDeselectNonSelectedShouldNotFireEvent() {
+      instance.deselectElements("E");
+
+      verify(mock, times(0)).propertyChange(eventCaptor.capture());
+   }
+
+   @Test
+   void testIsElementSelected() {
+      instance.selectElements("E");
+      assertTrue(instance.isElementSelected("E"));
+      assertFalse(instance.isElementSelected("A"));
    }
 }
